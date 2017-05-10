027e2418bd5e623f3eab0529dafe51ca42280b8a
Aravind Ramachandran <arramac@microsoft.com>
2017-05-10 00:28:12 -0700

    first cut

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var Gremlin = require('gremlin');
+var config = require("./config");
+
+const client = Gremlin.createClient(
+    443, 
+    config.endpoint, 
+    { 
+        "session": false, 
+        "ssl": true, 
+        "user": `/dbs/${config.database}/colls/${config.collection}`,
+        "password": config.primaryKey
+    });
+
+console.log('Running Drop');
+client.execute('g.V().drop()', { }, (err, results) => {
+  if (err) return console.error(err);
+  console.log(results);
+  console.log();
+
+//g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)
+
+  console.log('Running Add Vertex1'); 
+  client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)", { }, (err, results) => {
+    if (err) return console.error(err);
+    console.log(JSON.stringify(results));
+    console.log();
+    
+    console.log('Running Add Vertex2'); 
+    client.execute("g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)", { }, (err, results) => {
+      if (err) return console.error(err);
+      console.log(JSON.stringify(results));
+      console.log();
+
+      console.log('Running Add Edge'); 
+      client.execute("g.V('thomas').addE('knows').to(g.V('mary'))", { }, (err, results) => {
+        if (err) return console.error(err);
+        console.log(JSON.stringify(results));
+        console.log();
+        
+        console.log('Running Count'); 
+        client.execute("g.V().count()", { }, (err, results) => {
+          if (err) return console.error(err);
+          console.log(JSON.stringify(results));
+          console.log();
+        });
+      });
+      
+    });
+
+  });
+   
+});
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "GRAPHENDPOINT";
+config.primaryKey = "PRIMARYKEY";
+config.database = "graphdb"
+config.collection = "graphcoll"
+
+module.exports = config;
