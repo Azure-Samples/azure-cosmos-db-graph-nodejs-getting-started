e0469698500f39b4e5dcc3600ec033ab2acc0b60
Denny Lee <denny.g.lee@gmail.com>
2017-06-14 20:13:27 -0700

    Adding userID property
    Instructions specify a userid partitionKey, adding this property so adding vertices will work.

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ client.execute('g.V().drop()', { }, (err, results) => {
 //g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)
 
   console.log('Running Add Vertex1'); 
-  client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)", { }, (err, results) => {
+  client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('userid', 1)", { }, (err, results) => {
     if (err) return console.error(err);
     console.log(JSON.stringify(results));
     console.log();
     
     console.log('Running Add Vertex2'); 
-    client.execute("g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)", { }, (err, results) => {
+    client.execute("g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('userid', 2)", { }, (err, results) => {
       if (err) return console.error(err);
       console.log(JSON.stringify(results));
       console.log();
