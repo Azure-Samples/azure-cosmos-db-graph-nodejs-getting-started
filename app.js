a84b47657b041913d788913d16adfcb8db739667
LuisBosquez <luisdbosquez@gmail.com>
2017-11-10 11:51:01 -0800

    Updating app.js to use async and refactored into methods

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var Gremlin = require('gremlin');
 var config = require("./config");
+var async = require('async');
 
 const client = Gremlin.createClient(
     443, 
@@ -11,39 +12,78 @@ const client = Gremlin.createClient(
         "ssl": true, 
         "user": `/dbs/${config.database}/colls/${config.collection}`,
         "password": config.primaryKey
+    }
+);
+
+
+function dropGraph(callback)
+{
+    console.log('Running Drop');
+    client.execute('g.V().drop()', { }, (err, results) => {
+        if (err) callback(console.error(err));
+        console.log("Result: %s\n", JSON.stringify(results));
+        callback(null)
     });
+}
 
-console.log('Running Drop');
-client.execute('g.V().drop()', { }, (err, results) => {
-  if (err) return console.error(err);
-  console.log("Result: %s\n", JSON.stringify(results));
+function addVertex1(callback)
+{
+    console.log('Running Add Vertex1'); 
+    client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('userid', 1)", { }, (err, results) => {
+      if (err) callback(console.error(err));
+      console.log("Result: %s\n", JSON.stringify(results));
+      callback(null)
+    });
+}
 
-  console.log('Running Add Vertex1'); 
-  client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('userid', 1)", { }, (err, results) => {
-    if (err) return console.error(err);
-    console.log("Result: %s\n", JSON.stringify(results));
-    
+function addVertex2(callback)
+{
     console.log('Running Add Vertex2'); 
     client.execute("g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('userid', 2)", { }, (err, results) => {
-      if (err) return console.error(err);
+      if (err) calback(console.error(err));
       console.log("Result: %s\n", JSON.stringify(results));
-
-      console.log('Running Add Edge'); 
-      client.execute("g.V('thomas').addE('knows').to(g.V('mary'))", { }, (err, results) => {
-        if (err) return console.error(err);
-        console.log("Result: %s\n", JSON.stringify(results));
-        
-        console.log('Running Count'); 
-        client.execute("g.V().count()", { }, (err, results) => {
-          if (err) return console.error(err);
-          console.log("Result: %s\n", JSON.stringify(results));
-          console.log("Finished");
-        });
-
-      });
-      
+      callback(null)
     });
+}
 
-  });
-   
-});
+function addEdge(callback)
+{
+    console.log('Running Add Edge'); 
+    client.execute("g.V('thomas').addE('knows').to(g.V('mary'))", { }, (err, results) => {
+      if (err) callback(console.error(err));
+      console.log("Result: %s\n", JSON.stringify(results));
+      callback(null)
+    });
+}
+
+function countVertices(callback)
+{
+    console.log('Running Count'); 
+    client.execute("g.V().count()", { }, (err, results) => {
+      if (err) callback(console.error(err));
+      console.log("Result: %s\n", JSON.stringify(results));
+      callback(null)
+    });
+}
+
+function finish(callback)
+{
+    console.log("Finished");
+    console.log('Press any key to exit');
+    
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+}
+
+try{
+    async.waterfall([
+        dropGraph,
+        addVertex1,
+        addVertex2,
+        addEdge,
+        countVertices
+    ], finish);
+} catch(err) {
+    console.log(err)
+}
