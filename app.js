2fe239e4afd67b928ae4c3bb78e652ca2672d250
jbmusso <jbmusso+github@gmail.com>
2017-11-16 10:55:02 +0100

    Ensure callbacks are not called twice after handling an error

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ function dropGraph(callback)
 {
     console.log('Running Drop');
     client.execute('g.V().drop()', { }, (err, results) => {
-        if (err) callback(console.error(err));
+        if (err) {
+            return callback(console.error(err));
+        }
+
         console.log("Result: %s\n", JSON.stringify(results));
         callback(null)
     });
@@ -30,9 +33,12 @@ function addVertex1(callback)
 {
     console.log('Running Add Vertex1'); 
     client.execute("g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('userid', 1)", { }, (err, results) => {
-      if (err) callback(console.error(err));
-      console.log("Result: %s\n", JSON.stringify(results));
-      callback(null)
+        if (err) {
+            return callback(console.error(err));
+        }
+
+        console.log("Result: %s\n", JSON.stringify(results));
+        callback(null)
     });
 }
 
@@ -40,9 +46,12 @@ function addVertex2(callback)
 {
     console.log('Running Add Vertex2'); 
     client.execute("g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('userid', 2)", { }, (err, results) => {
-      if (err) calback(console.error(err));
-      console.log("Result: %s\n", JSON.stringify(results));
-      callback(null)
+        if (err) {
+            return calback(console.error(err));
+        }
+
+        console.log("Result: %s\n", JSON.stringify(results));
+        callback(null)
     });
 }
 
@@ -50,20 +59,26 @@ function addEdge(callback)
 {
     console.log('Running Add Edge'); 
     client.execute("g.V('thomas').addE('knows').to(g.V('mary'))", { }, (err, results) => {
-      if (err) callback(console.error(err));
-      console.log("Result: %s\n", JSON.stringify(results));
-      callback(null)
+        if (err) {
+            return callback(console.error(err));
+        }
+
+        console.log("Result: %s\n", JSON.stringify(results));
+        callback(null)
     });
 }
 
 function countVertices(callback)
 {
-    console.log('Running Count'); 
+    console.log('Running Count');
     client.execute("g.V().count()", { }, (err, results) => {
-      if (err) callback(console.error(err));
-      console.log("Result: %s\n", JSON.stringify(results));
-      callback(null)
-    });
+    if (err) {
+        return callback(console.error(err));
+    }
+
+    console.log("Result: %s\n", JSON.stringify(results));
+    callback(null)
+});
 }
 
 function finish(callback)
