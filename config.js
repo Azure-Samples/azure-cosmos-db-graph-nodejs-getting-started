34c482bda525be69d9211ae84df2bddb878fef22
Luis Bosquez <luisdbosquez@gmail.com>
2018-08-27 16:59:11 -0700

    Adding hostname format

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.endpoint = "GRAPHENDPOINT";
+config.endpoint = "GRAPHENDPOINT.gremlin.cosmosdb.azure.com";
 config.primaryKey = "PRIMARYKEY";
 config.database = "graphdb"
 config.collection = "graphcoll"
