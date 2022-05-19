40a5a4602c302ef9eb1e4a079b735c2cd00ce77e
Oliver Towers <olivert@microsoft.com>
2022-05-19 13:52:39 -0700

    Update config.js
    Replace cosmosdb -> cosmos in endpoint example.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.endpoint = "wss://DATABASE_ACCOUNT_NAME.gremlin.cosmosdb.azure.com:443/gremlin";
+config.endpoint = "wss://DATABASE_ACCOUNT_NAME.gremlin.cosmos.azure.com:443/gremlin";
 config.primaryKey = "PRIMARYKEY";
 config.database = "GRAPHDATABASE"
 config.collection = "GRAPHCOLLECTION"
